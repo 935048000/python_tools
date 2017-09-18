0abf286d2c4fa358841520e703b05f8746e6e221
935048000 <935048000@qq.com>
2017-09-18 11:24:52 +0800

    数据采集程序库

---
+++ collect/__init__.py
@@ -0,0 +1,20 @@
+import sys
+from collect.aep import collect
+from collect.version import __version__
+
+#python version > V2.6
+if sys.version_info < (2, 6):
+    raise RuntimeError('You need Python 2.6+ for this module.')
+
+__author__ = "chen.hao <chenhao159482@gmail.com>"
+__license__ = "GNU Lesser General Public License (LGPL)"
+
+
+__all__ = ['aep',]
+
+
+
+
+
+
+
