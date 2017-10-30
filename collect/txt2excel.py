61facb3a0a413ac6854035d053b0f0fe2c4b9645
935048000 <935048000@qq.com>
2017-10-30 15:13:09 +0800

    update

--- collect/txt2excel.py
+++ collect/txt2excel.py
@@ -9,9 +9,9 @@ from os import getcwd,path,mkdir
 
 class dataswitch ():
     # 将服务器的数据转存本地
-    def SFile2CFile(self, hostname, port, username, password):
+    def SFile2CFile(self, hostname, port, username, password,date):
         global DATAPATH
-        NOW_DATE = strftime ("%Y%m", localtime ())  # 取当前年月
+        #NOW_DATE = strftime ("%Y%m", localtime ())  # 取当前年月
         WORKPATH = getcwd ()  # 执行脚本的当前目录路径
         DATAPATH = WORKPATH + "\data"  # 执行脚本当前路径下的data目录
         if path.exists(DATAPATH): # 目录不存在就创建
@@ -19,9 +19,9 @@ class dataswitch ():
         else:
             mkdir(DATAPATH)
 
-        a = collect.collect ()
+        a = aep.collect ()
         a.connect (hostname, port, username, password)
-        CMD1 = "ls ~/log/Check/*.%s.dat" % (NOW_DATE)  # 根据年月查找对应的日志命令
+        CMD1 = "ls ~/log/Check/*.%s*.dat" % (date)  # 根据年月查找对应的日志命令
         SFile = a.command (CMD1, "notitle")
         SFile = SFile.split ('\n')  # 获取文件列表，带路径
         CFileNameTEMP = []  # 用于函数返回的文件名
@@ -39,10 +39,26 @@ class dataswitch ():
 
     # 将本地的数据文本转化为Excel表格并绘折线图
     def txt2xlsx(self,txtfile, xlsxfile):
-        # path = getcwd()
         datafile = txtfile  # 数据文件名
         xltfile = "%s\%s"%(DATAPATH,xlsxfile)  # xlsx文件名
         tablename = txtfile[-13:-7]  # 取文件的主要名字作为表名
+        if tablename == "888200":
+            tablename = "SSQ期汇总".decode('utf-8')
+        elif tablename == "900001":
+            tablename = "日汇总".decode('utf-8')
+        elif tablename == "900602":
+            tablename = "数据分析".decode('utf-8')
+        elif tablename == "900999":
+            tablename = "数据备份".decode('utf-8')
+        elif tablename == "913713":
+            tablename = "快三开奖".decode('utf-8')
+        elif tablename == "913714":
+            tablename = "快三摇奖".decode('utf-8')
+        else:
+            tablename = "SSQ检索".decode('utf-8')
+            xltfile = "%s\%s"%(DATAPATH,"888300.xlsx")
+        #tablename = "测试".decode('utf-8')
+        #print tablename
 
         # 数据行数
         f = open (datafile, 'r')
@@ -56,7 +72,7 @@ class dataswitch ():
         f.close ()
 
         work = xlsxwriter.Workbook (xltfile)
-        worksheet = work.add_worksheet (tablename)
+        worksheet = work.add_worksheet ()
 
         f = open (datafile, 'r')
         row = 0
@@ -73,8 +89,8 @@ class dataswitch ():
             f.close ()
 
             chartdict = {'categories': '=Sheet1!$A$1:$A$10', 'values': '=Sheet1!$C$1:$C$10'}
-            chartdict['categories'] = '=%s!$A$1:$A$%d' % (tablename, dataL)
-            chartdict['values'] = '=%s!$C$1:$C$%d' % (tablename, dataL)
+            chartdict['categories'] = '=%s!$A$1:$A$%d' % ("Sheet1", dataL)
+            chartdict['values'] = '=%s!$C$1:$C$%d' % ("Sheet1", dataL)
         else:
             for i in f.readlines ():  # 生成表格
                 l = i.strip ("\n").split ("\t")
@@ -87,8 +103,8 @@ class dataswitch ():
             f.close ()
 
             chartdict = {'categories': '=Sheet1!$C$1:$C$10', 'values': '=Sheet1!$D$1:$D$10'}
-            chartdict['categories'] = '=%s!$C$1:$C$%d' % (tablename, dataL)
-            chartdict['values'] = '=%s!$D$1:$D$%d' % (tablename, dataL)
+            chartdict['categories'] = '=%s!$C$1:$C$%d' % ("Sheet1", dataL)
+            chartdict['values'] = '=%s!$D$1:$D$%d' % ("Sheet1", dataL)
         # 绘图
         chartname = {'name': 'chartname'}
         chartname['name'] = '%s' % (tablename)
